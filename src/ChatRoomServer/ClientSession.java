4ab7e4b20179e58fd0dc6618775175199f35dfea
Ole <olemikole@hotmail.no>
2018-04-13 14:51:10 +0200

    Initial commit

---
+++ src/ChatRoomServer/ClientSession.java
@@ -0,0 +1,63 @@
+package ChatRoomServer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ClientSession implements Runnable {
+	ServerMain server;
+
+	BufferedReader in;
+	PrintWriter out;
+
+	String username;
+	final String LOGOUT = "/logout";
+
+	ClientSession(BufferedReader _in, PrintWriter _out, ServerMain _server) throws IOException {
+
+		server = _server;
+		in = _in;
+		out = _out;
+
+	}
+
+	@Override
+	public void run() {
+		String inputMessage;
+		boolean running = true;
+
+		try {
+
+			System.out.println("New client has arrived");
+
+			username = in.readLine();
+			System.out.println(username + " is ready to chat");
+			out.println("#############\nWELCOME TO SERVER");
+			out.flush();
+
+			while (running) {
+				inputMessage = in.readLine();
+				System.out.println(username + ": " + inputMessage);
+				if (inputMessage.equals(LOGOUT))
+					running = false;
+				else {
+					server.Send(inputMessage, this);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		} finally {
+			server.removeMyReference(this);
+			System.out.println(username + " disconnected");
+			server.Send("/logout", this);
+			System.out.println(ServerMain.clientSessions.size() + " people on the server");
+		}
+	}
+
+	public void Send(String message) {
+		out.println(message);
+		out.flush();
+	}
+
+}
---
+++ src/ChatRoomServer/ServerMain.java
@@ -0,0 +1,80 @@
+package ChatRoomServer;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ServerMain {
+
+    ServerSocket serverSocket;
+    Socket currentClientSocket;
+    ClientSession currentClient;
+    BufferedReader br;
+    BufferedReader in;
+    PrintWriter out;
+
+    public static ArrayList<ClientSession> clientSessions = new ArrayList<ClientSession>();
+
+    private ServerMain(int port) {
+        try {
+            serverSocket = new ServerSocket(port);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        new ServerMain(61000).startServer();
+    }
+
+    private void startServer() {
+
+        System.out.println("Server is ready");
+        try {
+            while (true) {
+
+                currentClientSocket = serverSocket.accept();
+                out = new PrintWriter(currentClientSocket.getOutputStream());
+                in = new BufferedReader(new InputStreamReader(currentClientSocket.getInputStream()));
+                currentClient = new ClientSession(in, out, this);
+
+                new Thread(currentClient).start();
+
+                clientSessions.add(currentClient);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void Send(String message, ClientSession user) {// sends to all clients except sender
+        synchronized (clientSessions) {
+            String s;
+            if (message.equals("/logout"))
+                s = user.username + " has disconnected!";
+            else
+                s = user.username + ": " + message;
+            for (ClientSession cl : clientSessions)
+                cl.Send(s);
+
+        }
+
+    }
+
+    public void removeMyReference(ClientSession cl)// removes the reference to a session
+    {
+        synchronized (clientSessions) {
+            clientSessions.remove(clientSessions.indexOf(cl));
+        }
+
+    }
+}
